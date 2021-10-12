d3080634c40794c029a2dd9b1fe6dd3fc3b4e35d
Martin Peters <mjpeters@btinternet.com>
2021-10-12 22:19:25 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    Pixel_Array.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Red))
-    Pixel_Array.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
-    Pixel_Array.range(3, 2).showColor(neopixel.colors(NeoPixelColors.Blue))
+    Pixel_Array.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    Pixel_Array.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    Pixel_Array.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    Pixel_Array.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    Pixel_Array.setPixelColor(4, neopixel.colors(NeoPixelColors.Purple))
 })
 input.onButtonPressed(Button.B, function () {
     Pixel_Array.clear()
@@ -9,3 +11,8 @@ input.onButtonPressed(Button.B, function () {
 })
 let Pixel_Array: neopixel.Strip = null
 Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+basic.forever(function () {
+    basic.pause(100)
+    Pixel_Array.rotate(1)
+    Pixel_Array.show()
+})
