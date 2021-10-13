bed3b30b19d8a9288cfb76b420e97f6d86c96d11
Martin Peters <mjpeters@btinternet.com>
2021-10-13 23:10:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,19 @@
 input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 180)
     Pixel_Array.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     Pixel_Array.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
     Pixel_Array.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
     Pixel_Array.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
     Pixel_Array.setPixelColor(4, neopixel.colors(NeoPixelColors.Purple))
 })
+input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+})
 input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 0)
     Pixel_Array.clear()
     Pixel_Array.show()
 })
